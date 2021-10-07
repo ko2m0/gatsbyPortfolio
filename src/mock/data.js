53ec52adad3a118d2605a5f3e8e13e305fecced0
Ko2m0 <kozmopimp@gmail.com>
2021-10-07 18:14:53 -0500

    personal data added

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,29 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Welcome to my site', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Kozmo',
+  subtitle: 'Web/graphic Designer based in Morelia, Mexico.',
+  cta: 'more about me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'I have a bachelor’s degree in Journalism and Communication and more than ten years of experience in Visual Communication; for the past nine years I have developed my skills in graphic and web design working both in private and government companies and also abroad.',
+  paragraphTwo:
+    'As an aspiring visual artist and whimsical wavemaker, I enjoy working with multimedia tools to communicate ideas that later become marketing tools for successful products and services. As a leader I’ve had the pleasure of managing small groups and creating special and caring collaborations through Agile methodologies that build excellent results.',
+  paragraphThree:
+    'As a journalism enthusiast I’m fond of the art of the written word. I enjoy writing as much as I enjoy reading. A few years ago I worked as an English language instructor for top-notch companies',
+  resume: 'https://drive.google.com/file/d/1P7Utl5ZTGq17xFkT3_PxOCt36yZL6nok/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,20 +32,20 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'La Casa del Aguacate',
+    info: 'Designed & coded',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://casadelaguacate.com',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
+    title: 'Karnak. Temple of the gods',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/ko2m0/karnak', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -58,8 +61,8 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Email me',
+  email: 'kozmopimp@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +71,29 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/kozmo_',
     },
+    /*
     {
       id: nanoid(),
       name: 'codepen',
       url: '',
     },
+    */
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/kozmoromero',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/ko2m0',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
